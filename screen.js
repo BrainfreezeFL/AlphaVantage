59639da3ab804f57866778be66395c4f94801cd4
BrainfreezeFL <samuel.lewis1@gmail.com>
2018-08-04 01:05:59 -0400

    Is able to read in a list of ticker symbols and fetch all of the data and make it usable. Still needs to implement a screener to pick the stocks we want

--- screen.js
+++ screen.js
@@ -1,6 +1,6 @@
 // Author: Samuel Lewis
 // Date: 8/3/2018
-// This is the starting point of the project
+
 var https = require("https");
 var fs = require('fs');
 var http = require('http');
@@ -11,38 +11,55 @@ var fs = require('fs');                        // Require Node.js FileSystem API
 const hist = require("./historical_data");
 const page = require("./page_number");
 const stitch = require("./url_stitcher");
+const screen = require("./screener");
 
-// This is my API key
+// This is my personal API key
+// For other users please go to the AlphaVantage site and get a free API key for personal use to avoid reaching API call limits prematurely on this key
 var key = "QDYU0UP3VQXY275F";
 
-
-// This function will be called back after the first http request made
-
-
 function main() {
+    // This array is the list of stock symbols that you want to screen for information
     var array = ["AAPL", "MSFT"];
-    //for(i=0;i<array.Length;i++){
-    var url = stitch.url_stitcher(array[1]);
-    https.get(url, (resp) => {
-  let data = '';
- 
-  // A chunk of data has been recieved.
-  resp.on('data', (chunk) => {
-    data += chunk;
-  });
- 
-  // The whole response has been received. Print out the result.
-  resp.on('end', () => {
-      var parsed = hist.historical_data(data);
-      console.log(hist.historical_data(data));
-     // console.log(data);
-  });
- 
-}).on("error", (err) => {
-  console.log("Error: " + err.message);
-});
-   // }
-}
     
+    // This variable is needed because of nodes asynchronous way of handling programs. It will loop through the for loop before the setTimeout code finishes, which means that the variable i will not exist when the setTimeout function needs it, So it look for info in the array using a variable that doesnt exist anymore. By increasing tempy each time the setTimeout function is called then we will always look at the right place in the array.
+    var tempy = 0;
+    
+    // Make the for loop to call the setTimeout function the same amount of times as elements in the list
+    for(i=0;i<array.length;i++){
+        
+        // The setTimeout function will make all of the API calls and parse through all the data using its minimum time of 13 seconds. The API has a limit of 5 calls per minute for free users, so the time delay gives the program processing time and prevents it from hitting the limit.
+        setTimeout(() => {
+            var url = stitch.url_stitcher(array[tempy]);
+            https.get(url, (resp) => {
+                let data = '';
+ 
+                // A chunk of data has been recieved.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+ 
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    // Increase tempy to keep track of what ticker we are using.
+                    tempy = tempy+1;
+                    
+                    // Parse the data to make it usable
+                    var parsed = hist.historical_data(data);
+                    
+                    // Perform the screen on the data
+                    // screen.twenty(parsed);
+                });
+                
+                
+            }).on("error", (err) => {
+                console.log("Error: " + err.message);
+            });
+            
+        // Time between API calls
+        }, 12000 * i + 1000);
+    }
 
+}
+      
+// Run the project
 main();
